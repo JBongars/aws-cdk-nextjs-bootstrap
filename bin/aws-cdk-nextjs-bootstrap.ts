d01edda9b8d5a1577516166c47270d85daf5cfe4
julien bongars <julienbongars23@gmail.com>
2023-02-08 05:14:12 +0800

    fix: added stack to app

--- bin/aws-cdk-nextjs-bootstrap.ts
+++ bin/aws-cdk-nextjs-bootstrap.ts
@@ -3,10 +3,18 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { AwsCdkPipelineStack } from "../lib/aws-cdk-pipeline-stack";
 import config from "../config";
+import { NextJsAppStack } from "../lib/nextjs-app-stack";
 
 const app = new cdk.App();
 
-new AwsCdkPipelineStack(app, "AppBootstrapStack", {
+new AwsCdkPipelineStack(app, "AppPipelineStack", {
+  env: {
+    account: config.aws.account_id,
+    region: config.aws.region.az1,
+  },
+});
+
+new NextJsAppStack(app, "AppNextJsAppStack", {
   env: {
     account: config.aws.account_id,
     region: config.aws.region.az1,
