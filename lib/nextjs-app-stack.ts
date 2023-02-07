6aab754e611ad169934fa246f34dcf2f8ef57075
julien bongars <julienbongars23@gmail.com>
2023-02-08 05:02:17 +0800

    fix: codeBuilder

--- lib/nextjs-app-stack.ts
+++ lib/nextjs-app-stack.ts
@@ -1,21 +1,47 @@
 ﻿import * as cdk from "aws-cdk-lib";
 import * as codeBuild from "aws-cdk-lib/aws-codebuild";
 import { Construct } from "constructs";
-import { config } from "dotenv";
+import config from "../config";
 
 export class NextJsAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new codeBuild.Project(this, "MyProject", {
+    new codeBuild.Project(this, "TestBuilder", {
+      projectName: "TestBuilder",
       buildSpec: codeBuild.BuildSpec.fromObject({
         version: "0.1",
         phases: {
           build: {
-            commands: ['echo "Hello, CodeBuild!"'],
+            commands: ["echo Hello world!"],
           },
         },
       }),
     });
+    // new codeBuild.Project(this, "AppBuilder", {
+    //   projectName: "AppBuilder",
+    //   source: codeBuild.Source.gitHub({
+    //     owner: config.app_repo.owner,
+    //     repo: config.app_repo.name,
+    //     webhook: true,
+    //     webhookTriggersBatchBuild: true,
+    //     webhookFilters: [
+    //       codeBuild.FilterGroup.inEventOf(
+    //         codeBuild.EventAction.PUSH
+    //       ).andBranchIs("master"),
+    //     ],
+    //   }),
+    //   buildSpec: codeBuild.BuildSpec.fromObject({
+    //     version: "0.1",
+    //     phases: {
+    //       build: {
+    //         commands: [
+    //           "yarn install --prefer-offline --frozen-lockfile --non-interactive --production=false",
+    //           "yarn build",
+    //         ],
+    //       },
+    //     },
+    //   }),
+    // });
   }
 }
