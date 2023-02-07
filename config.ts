4f826fdd5a82776c162a774104ed4f46ab4617ee
julien bongars <julienbongars23@gmail.com>
2023-02-07 12:07:19 +0800

    feat: infra code pipeline

---
+++ config.ts
@@ -0,0 +1,24 @@
+﻿import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+  aws: {
+    account_id: process.env.AWS_ACCOUNT_ID || "",
+    region: {
+      az1: process.env.AWS_REGION_AZ1 || "",
+    },
+  },
+  app_repo: {
+    owner: "JBongars",
+    name: "nextjs-template",
+    url: process.env.APP_REPO_URL || "",
+  },
+  infra_repo: {
+    owner: "JBongars",
+    name: "aws-cdk-nextjs-bootstrap",
+    url: process.env.INFRA_REPO_URL || "",
+  },
+};
+
+export default config;
---
+++ lib/aws-cdk-pipeline-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from "aws-cdk-lib";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+import config from "../config";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AwsCdkPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new CodePipeline(this, "Pipeline", {
+      pipelineName: "Nextjs-App-Pipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          `${config.infra_repo.name}/${config.infra_repo}`,
+          "main"
+        ),
+        commands: ["yarn", "yarn build", "npx cdk synth"],
+      }),
+    });
+  }
+}
