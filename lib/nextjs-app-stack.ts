b59ddead74fde060210dd93770e3438265137193
julien bongars <julienbongars23@gmail.com>
2023-02-08 04:48:45 +0800

    feat: app code build spec

---
+++ lib/nextjs-app-stack.ts
@@ -0,0 +1,21 @@
+﻿import * as cdk from "aws-cdk-lib";
+import * as codeBuild from "aws-cdk-lib/aws-codebuild";
+import { Construct } from "constructs";
+import { config } from "dotenv";
+
+export class NextJsAppStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new codeBuild.Project(this, "MyProject", {
+      buildSpec: codeBuild.BuildSpec.fromObject({
+        version: "0.1",
+        phases: {
+          build: {
+            commands: ['echo "Hello, CodeBuild!"'],
+          },
+        },
+      }),
+    });
+  }
+}
